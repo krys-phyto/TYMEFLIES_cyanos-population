c99dd315e5b6a61cab33e46738bbf2d695d85a3f
Krys Kibler <kjkibler@wisc.edu>
2025-09-03 11:21:23 -0500

    files for 50 cyano-motus

---
+++ final_50_tree/filtered_52_cyanomOTUs.sh
@@ -0,0 +1,129 @@
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/CYBIM_31.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/CYBIM_89.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/CYBIM_93.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/PSEUDA_157.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/CYBIM_119.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/VULCA_20.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/CYBIM_52.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/CYANO_45.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/CYBIM_73.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/CYBIM_160.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/CYBIM_15.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/CYBIM_190.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/CYBIM_101.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/DOLIS_105.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/MCYST_31.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/CYBIM_94.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/CYBIM_104.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/CYBIM_157.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/CYBIM_119_1.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/CYBIM_77.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/PSEUDA_281.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/PSEUDA_13.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/APHAN_134.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/MCYST_56.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/CYBIM_63.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/CYBIM_76.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/CYBIM_73_1.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/DOLIS_187.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/VAMPV_261.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/CYBIM_22.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/CYBIM_60_1.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/CYBIM_80.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/VAMPV_156.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/CYBIM_39.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/MCYST_62.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/MCYST_2.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/VULCA_28.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/CYBIM_51.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/CYBIM_200.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/VULCA_96.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/CYBIM_28.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/CYANO_106.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/CYBIM_90.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/VULCA_120.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/NODOS_103.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/NODOS_105.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/CYANO_51_1.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/PSEUDA_70.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/CYBIM_136_1.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/CYBIM_130_1.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/CYANO_84.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+cp /mnt/bigdata/linuxhome/trina.mcmahon/TYMEFLIES/data/cyano_diamond_76_fastas/PSEUDA_31.fna /home/glbrc.org/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs/
+
+#################################################################
+cd /mnt/bigdata/linuxhome/kjkibler/cyanoSTRONG/krys/analysis/final_46_tree/cyano_mOTUs
+
+
+# started in interactive condor
+conda activate anvio-7.1
+
+### Create contig db for every MAG
+# trimmed diamond set
+for i in *fna
+do
+    cyano=`basename $i`
+    anvi-gen-contigs-database -f $i -o ${cyano/%.fna}.db -T 4
+    anvi-run-hmms -c ${cyano/%.fna}.db
+done
+
+### Create external-genomes.txt
+# done with a combination of R and bash
+# file structure is tab delimited
+# name  contigs_db_path
+
+echo -e "name\tcontigs_db_path" > external-genomes.txt
+for i in *db
+do
+  cyano=${i/%.db}
+  echo -e "$cyano\t$i" >> external-genomes.txt
+done
+
+### Ensure scgs are there in each mOTU
+anvi-get-sequences-for-hmm-hits --external-genomes external-genomes.txt \
+                                   --hmm-source Bacteria_71 \
+                                   --list-available-gene-names
+
+### Spit out scg gene sequences for tree
+anvi-get-sequences-for-hmm-hits --external-genomes external-genomes.txt \
+                                -o concatenated-proteins.fa \
+                                --hmm-source Bacteria_71 \
+                                --gene-names Ribosomal_L1,Ribosomal_L13,Ribosomal_L14,Ribosomal_L16,Ribosomal_L17,Ribosomal_L18p,Ribosomal_L19,Ribosomal_L2,Ribosomal_L20,Ribosomal_L21p,Ribosomal_L22,Ribosomal_L23,Ribosomal_L27,Ribosomal_L27A,Ribosomal_L28,Ribosomal_L29,Ribosomal_L3,Ribosomal_L32p,Ribosomal_L35p,Ribosomal_L4,Ribosomal_L5,Ribosomal_L6,Ribosomal_L9_C,Ribosomal_S10,Ribosomal_S11,Ribosomal_S13,Ribosomal_S15,Ribosomal_S16,Ribosomal_S17,Ribosomal_S19,Ribosomal_S2,Ribosomal_S20p,Ribosomal_S3_C,Ribosomal_S6,Ribosomal_S7,Ribosomal_S8,Ribosomal_S9,ribosomal_L24 \
+                                --return-best-hit \
+                                --get-aa-sequences \
+                                --concatenate
+
+
+
+### Create tree (default is fasttree)
+anvi-gen-phylogenomic-tree -f concatenated-proteins.fa \
+                           -o TYMEFLIES_cyano-mOTUs_tree.txt
+
+#Alignment sequence length ....................: 6,583
+#Version ......................................: FastTree Version 2.1.11 Double precision (No SSE3)
+#Alignment ....................................: standard input
+#Info .........................................: Amino acid distances: BLOSUM45 Joins: balanced Support: SH-like 1000
+#Search .......................................: Normal +NNI +SPR (2 rounds range 10) +ML-NNI opt-each=1
+#TopHits ......................................: 1.00*sqrtN close=default refresh=0.80
+#ML Model .....................................: Jones-Taylor-Thorton, CAT approximation with 20 rate categories
+#Info .........................................: Ignored unknown character X (seen 5772 times)
+#Refining topology ............................: 23 rounds ME-NNIs, 2 rounds ME-SPRs, 11 rounds ML-NNIs
+#Info .........................................: Total branch-length 2.745 after 3.56 sec
+#ML-NNI round 1 ...............................: LogLk = -112114.167 NNIs 7 max delta 25.65 Time 13.23
+#Info .........................................: Switched to using 20 rate categories (CAT approximation)
+#Info .........................................: Rate categories were divided by 0.849 so that average rate = 1.0
+#Info .........................................: CAT-based log-likelihoods may not be comparable across runs
+#Info .........................................: Use -gamma for approximate but comparable Gamma(20) log-likelihoods
+#ML-NNI round 2 ...............................: LogLk = -102060.768 NNIs 4 max delta 4.41 Time 20.36
+#ML-NNI round 3 ...............................: LogLk = -102060.099 NNIs 0 max delta 0.00 Time 22.10
+#Info .........................................: Turning off heuristics for final round of ML NNIs (converged)
+#ML-NNI round 4 ...............................: LogLk = -102045.657 NNIs 1 max delta 1.50 Time 28.47 (final)
+#Optimize all lengths .........................: LogLk = -102044.947 Time 30.41
+
+# report out mag stats
+for f in *.db
+do
+anvi-display-contigs-stats $f \
+                          --report-as-text \
+                          -o $f.txt
+done
